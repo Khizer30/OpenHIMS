e08e0850f7f3082970d8fb9d289d0f690f6866a0
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2024-04-07 00:33:56 +0500

    v0.26
    *Charts Updated

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -9,6 +9,16 @@ Chart.register(ArcElement, Tooltip, Legend, PointElement, LineElement, CategoryS
 // Dashboard
 export default function Dashboard(): JSX.Element
 {
+  const colors: string[] =
+    [
+      "#00BFFF",
+      "#14A6E1",
+      "#0080C0",
+      "#3366CC",
+      "#1A5AA1",
+      "#004080"
+    ];
+
   return (
     <>
       <div className=" my-4 h-2/5 flex justify-center items-center">
@@ -22,7 +32,13 @@ export default function Dashboard(): JSX.Element
           <Doughnut
             data={ {
               labels: ["Consultation", "ECG", "Injection", "Acupuncture", "Procedure", "Nebulizing"],
-              datasets: [{ label: "Revenue", data: [100, 200, 300, 400, 500, 600] }]
+              datasets:
+                [{
+                  label: "Revenue",
+                  data: [100, 200, 300, 400, 500, 600],
+                  borderColor: colors,
+                  backgroundColor: colors
+                }]
             } }
           />
         </div>
@@ -33,8 +49,15 @@ export default function Dashboard(): JSX.Element
         <div className=" w-full h-full mx-4 p-4 flex justify-center items-center rounded-lg shadow shadow-gray-200 bg-gray-50">
           <Line
             data={ {
-              labels: ["01/04/2024", "02/04/2024", "03/04/2024", "04/04/2024", "05/04/2024"],
-              datasets: [{ label: "Revenue", data: [4000, 6000, 12000, 2000, 8000] }]
+              labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+              datasets:
+                [{
+                  label: "Revenue",
+                  data: [4000, 6000, 12000, 2000, 8000, 6000],
+                  tension: 0.2,
+                  borderColor: colors[0],
+                  backgroundColor: colors[0]
+                }]
             } }
           />
         </div>
